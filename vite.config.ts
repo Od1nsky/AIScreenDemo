3892b9e9e816aecf574e29bb5d2e16b3c38428e4
v.kapustian <vladislav.kapustyan@immelman.ru>
2025-05-21 11:25:25 +0300

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/untitled/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -18,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html')
-      },
+    },
       output: {
         manualChunks: undefined,
         entryFileNames: 'assets/[name].js',
